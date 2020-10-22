4c46145587760165f093f3974e5c6a61f3630b1b
Julia <j.schenk@htlkrems.at>
2020-10-22 13:43:23 +0200

    hua

--- AernauticaImperialis/Program.cs
+++ AernauticaImperialis/Program.cs
@@ -7,7 +7,7 @@ namespace AernauticaImperialis
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            Console.WriteLine();
         }
     }
 }
